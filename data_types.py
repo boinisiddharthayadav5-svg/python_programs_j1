54c48f493912191853d057f409fc304a3278c757
boinisiddharthayadav5-svg <boinisiddharthayadav5@gmail.com>
2025-11-22 09:48:03 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types - in python 3 data types
+#.int(integer)
+#2.str(String)
+#3.float(decimals)
+name="siddhu3913"
+age=30
+salary=10000.52
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
---
+++ variables.py
@@ -0,0 +1,7 @@
+#variables-is a memory allocation to a value
+name="siddhu"
+age=31
+salary=1000.00
+print(name)
+print(age)
+print(salary)
